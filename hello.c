16794042acfd53aa2ca585ad0079254ffb53b636
Archer0016 <aantonson2@huskers.unl.edu>
2026-08-27 12:58:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Archer Antonson
+ *         aantonson2@huskers.unl.edu
+ * Date: 2026/27/08
+ *
+ * A program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Archer Antonson\n");
+
+  return 0;
+}
